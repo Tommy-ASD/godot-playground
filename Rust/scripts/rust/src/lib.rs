1ae8aa4e60075d1522f604eebcf551b818e358b1
Tommy-ASD <82094450+Tommy-ASD@users.noreply.github.com>
2023-10-08 16:49:02 +0200

    this has become my godot playground

--- Rust/scripts/rust/src/lib.rs
+++ Rust/scripts/rust/src/lib.rs
@@ -1,7 +1,8 @@
 use godot::{
-    engine::{Sprite2D, Sprite2DVirtual},
+    engine::{CollisionShape2D, RigidBody2D, RigidBody2DVirtual, Sprite2D, Texture2D},
     prelude::{
-        gdextension, godot_api, godot_print, Base, ExtensionLibrary, GodotClass, Input, Vector2,
+        gdextension, godot_api, godot_print, load, Base, ExtensionLibrary, Gd, GodotClass, Input,
+        Node, Vector2,
     },
 };
 
@@ -11,42 +12,47 @@ struct MyExtension;
 unsafe impl ExtensionLibrary for MyExtension {}
 
 #[derive(GodotClass)]
-#[class(base=Sprite2D)]
+#[class(base=RigidBody2D)]
 struct Player {
     speed: f64,
 
     #[base]
-    sprite: Base<Sprite2D>,
+    sprite: Base<RigidBody2D>,
 }
 
 #[godot_api]
-impl Sprite2DVirtual for Player {
-    fn init(sprite: Base<Sprite2D>) -> Self {
+impl RigidBody2DVirtual for Player {
+    fn init(sprite: Base<RigidBody2D>) -> Self {
         godot_print!("Hello, world!"); // Prints to the Godot console
 
-        Self {
-            speed: 400.0,
-            sprite,
-        }
+        Self { speed: 1.0, sprite }
     }
 
     fn physics_process(&mut self, delta: f64) {
         let input = Input::singleton();
+
+        let collisions = self.sprite.get_colliding_bodies();
+
+        let translation;
 
         match (
             input.is_action_pressed("ui_right".into()),
             input.is_action_pressed("ui_left".into()),
         ) {
             (true, false) => {
-                self.sprite
-                    .translate(Vector2::RIGHT * self.speed as f32 * delta as f32);
+                translation = Vector2::RIGHT;
                 godot_print!("ui_right pressed")
             }
             (false, true) => {
+                translation = Vector2::LEFT;
                 godot_print!("ui_left pressed")
             }
-            _ => {} // if neither or both are pressed
+            _ => {
+                translation = Vector2::ZERO;
+            } // if neither or both are pressed
         }
+        self.sprite
+            .translate(translation * self.speed as f32 * delta as f32);
     }
 }
 
